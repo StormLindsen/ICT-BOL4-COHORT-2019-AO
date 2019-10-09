dc27121fb7c4eaef7f77f913a7446568258c2c0c
StormLindsen <99059075@mydavinci.nl>
2019-10-09 10:52:56 +0200

    Gestart met opdracht

---
+++ WEEK 4/The Game/assets/js/TheGame.js
@@ -0,0 +1,9 @@
+var Choice1 = prompt("You wake up in a forest on a deserted island and you immediately get attacked by a bear what do you do? \r\n 1: You run away to a cave you see in the distance and hide there.\r\n 2: You fight the bear.")
+
+    if(Choice1 == 1){
+        alert("You survived by running away and hiding in a cave.")
+        
+}else{
+   alert("The bear kills you. (What else did you think would happen?") 
+
+}
